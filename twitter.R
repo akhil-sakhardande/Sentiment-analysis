47b635deb6abd8c3406fd8e49e082d4f6e67b7d8
akhil-sakhardande <akhil.sakhardande@gmail.com>
2015-12-11 16:04:49 +0530

    New R file

---
+++ twitter.R
@@ -0,0 +1,52 @@
+rm(list=ls())
+
+require("twitteR")||install.packages("twitteR")
+library(twitteR)
+
+#############################################
+# Authentication
+#############################################
+
+api_key <- "pL03JAZr8qGYezHi6z4BAbWQe"   #Consumer key: *
+
+api_secret <- "FeH2y6SYQJxcqIPXmmwwfMMNkIKNrnfZluCYrRRtV7QRQL9Ig3"   # Consumer secret: *
+
+access_token <- "55590423-br5X9sCQnQzoqfEQxi6B9SfZ8JGMlzTnjYbeKhaUY"  # Access token: 
+
+access_token_secret <- "BKeK8DKixWYwS1ljOHPuEfjau9xsuGZHbau0e2IguUSLo" # Access token secret: 
+
+# After this line of command type 1 for selection as Yes 
+setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
+
+#[1] "Using direct authentication"
+#Use a local file to cache OAuth access credentials between R sessions?
+#1: Yes
+#2: No
+#Selection: 1
+
+#############################################
+# Extract Tweets
+#############################################
+
+# hash tag for tweets search and number of tweets
+tweets = searchTwitter('#hero4', n=750 ) #Extracting from twitter
+
+tweets = twListToDF(tweets)    # Convert from list to dataframe
+
+tweets.df = tweets[,1]  # assign tweets for cleaning
+
+tweets.df = gsub("(RT|via)((?:\\b\\W*@\\w+)+)", "", tweets.df);head(tweets.df) 
+
+tweets.df = gsub("@\\w+", "", tweets.df);head(tweets.df) # regex for removing @user
+tweets.df = gsub("[[:punct:]]", "", tweets.df);head(tweets.df) # regex for removing punctuation mark
+tweets.df = gsub("[[:digit:]]", "", tweets.df);head(tweets.df) # regex for removing numbers
+tweets.df = gsub("http\\w+", "", tweets.df);head(tweets.df) # regex for removing links
+tweets.df = gsub("\n", " ", tweets.df);head(tweets.df)  ## regex for removing new line (\n)
+tweets.df = gsub("[ \t]{2,}", " ", tweets.df);head(tweets.df) ## regex for removing two blank space
+tweets.df =  gsub("[^[:alnum:]///' ]", " ", tweets.df)     # keep only alpha numeric 
+tweets.df =  iconv(tweets.df, "latin1", "ASCII", sub="")   # Keep only ASCII characters
+tweets.df = gsub("^\\s+|\\s+$", "", tweets.df);head(tweets.df)  # Remove leading and trailing white space
+
+tweets[,1] = tweets.df # save in Data frame
+
+write.table(tweets, file='GoPro_Hero4_tweets.txt', row.names=F, sep="\t")  # export to txt tab delemited file
